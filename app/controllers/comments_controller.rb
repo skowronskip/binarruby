654abf7116a52dbcdf208777bd3a896f57310339
Pawel Skowronski <210952@edu.p.lodz.pl>
2018-11-08 13:11:06 +0100

    Add new comment to choosen message form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,34 @@ class CommentsController < ApplicationController
     def index
       @comments = Comment.all
     end
+
+    def new 
+      @comment = Comment.new
+      @comment.message_id = Message.find(params[:message]).id
+    end
   
     def show
       @comment = Comment.find(params[:id])
+    end
+
+    def create 
+      @comment = Comment.new(comment_params)
+      if @comment.save
+        redirect_to @comment
+      else
+        render :new
+      end
     end
   
     def destroy
       comment = Comment.find(params[:id])
       comment.destroy
       redirect_to comments_path
+    end
+
+    private
+
+    def comment_params 
+      params.require(:comment).permit(:content, :author, :message_id)
     end
   end
